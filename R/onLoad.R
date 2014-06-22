0dd65196066385774bb6a4db07501d86ea835ce0
Jeroen <jeroenooms@gmail.com>
2014-06-22 13:17:02 -0700

    fix

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,5 @@
 .onLoad <- function(lib, pkg){
   #automatically loads the dataset when package is loaded
   #do not use this in combination with lazydata=true
-  data(tv_model, package = pkg, envir = parent.env(environment()))
+  utils::data(tv_model, package = pkg, envir = parent.env(environment()))
 }
